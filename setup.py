2bc9bea91a8927cecb54c669bd0f5bb85d16d256
pasteldew <PastelDew@gmail.com>
2019-05-17 08:40:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,66 @@
-from setuptools import setup, find_packages
+"""
+The build/compilations setup
+>> pip install -r requirements.txt
+>> python setup.py install
+"""
+import pip
+import logging
+import pkg_resources
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-#python3 setup.py build
-#python3 setup.py install
-#pip3 install -e .
-setup(name='PDStereo', version='1.0', packages=find_packages())
+
+def _parse_requirements(file_path):
+    pip_ver = pkg_resources.get_distribution('pip').version
+    pip_version = list(map(int, pip_ver.split('.')[:2]))
+    if pip_version >= [6, 0]:
+        raw = pip.req.parse_requirements(file_path,
+                                         session=pip.download.PipSession())
+    else:
+        raw = pip.req.parse_requirements(file_path)
+    return [str(i.req) for i in raw]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning('Fail load requirements file, so using default ones.')
+    install_reqs = []
+
+setup(
+    name='PDStereo',
+    version='1.0',
+    url='https://github.com/PastelDew/PDStereo',
+    author='PastelDew',
+    author_email='pasteldew@gmail.com',
+    license='MIT',
+    description='The program that is for detecting based on stereo images by using Mask R-CNN.',
+    packages=["mrcnn", "PDStereo", "PDStereo.Camera", "PDStereo.InjeAI", "PDStereo.QtApp"],
+    install_requires=install_reqs,
+    include_package_data=True,
+    python_requires='>=3.4',
+    long_description="""This project is for Deep learning using Mask R-CNN.
+    In this project, we use RGB-D images based on stereo image.""",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Image Segmentation",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords="stereo callibration matching image instance segmentation object detection mask rcnn r-cnn tensorflow keras",
+)
