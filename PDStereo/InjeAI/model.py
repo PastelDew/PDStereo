bf1be7651a676b810a95c23295eb68c27b6c14b0
pasteldew <PastelDew@gmail.com>
2019-06-15 05:01:39 +0900

    Bug Fix

--- PDStereo/InjeAI/model.py
+++ PDStereo/InjeAI/model.py
@@ -504,7 +504,7 @@ def data_generator(dataset, config, shuffle=True, augment=False, augmentation=No
                     batch_images = np.zeros(
                         (batch_size,) + image.shape[:2] + (3,), dtype=np.float32)
                     batch_depth_images = np.zeros(
-                        (batch_size,) + (1,), dtype=np.float32
+                        (batch_size,) + image.shape[:2] + (1,), dtype=np.float32
                     )
                 else:
                     batch_images = np.zeros(
